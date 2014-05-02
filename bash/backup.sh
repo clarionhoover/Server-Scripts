28f90cfed8c7a45eea6168a2ccbcae10177c6d27
Randy Hoover <randy.hoover@gmail.com>
2014-05-02 14:59:39 +0000

    fixed how it sshs and fixed issues with improper quote usage to pass variables

--- bash/backup.sh
+++ bash/backup.sh
@@ -23,7 +23,7 @@ DATE="$YEAR-$MONTH-$DAY"
 
 # start with the files
 echo "$(date +"%D %r") - Beginning webroot tarball"
-tar -czf $LOCALSTORE/webroot-backups_$DATE.tar.gz $WEBROOT && echo "$(date +"%D %r") - webroot tarball complete" || echo "$(date +"%D %r") - webroot tarball failed" 
+tar -czPf $LOCALSTORE/webroot-backups_$DATE.tar.gz $WEBROOT && echo "$(date +"%D %r") - webroot tarball complete" || echo "$(date +"%D %r") - webroot tarball failed" 
 # finished files
 
 # now to the database
@@ -35,11 +35,11 @@ done
 # finished dumping sql files
 
 # time to tar up the sql files
-tar -czf $LOCALSTORE/mysql-backups_$DATE.tar.gz $LOCALSTORE/*.$DATE.sql && echo "$(date +"%D %r") - sql tarball complete" || echo "$(date +"%D %r") - sql tarball failed"
+tar -czPf $LOCALSTORE/mysql-backups_$DATE.tar.gz $LOCALSTORE/*.$DATE.sql && echo "$(date +"%D %r") - sql tarball complete" || echo "$(date +"%D %r") - sql tarball failed"
 # tarred
 
 # time to tar the web backups and sql
-tar -czf $LOCALSTORE/backup_$DATE.tar.gz $LOCALSTORE/webroot-backups_$DATE.tar.gz $LOCALSTORE/mysql-backups_$DATE.tar.gz && echo "$(date +"%D %r") - full tarbll complete" || echo "$(date +"%D %r") - full tarball failed"
+tar -czPf $LOCALSTORE/backup_$DATE.tar.gz $LOCALSTORE/webroot-backups_$DATE.tar.gz $LOCALSTORE/mysql-backups_$DATE.tar.gz && echo "$(date +"%D %r") - full tarbll complete" || echo "$(date +"%D %r") - full tarball failed"
 
 # time to clean up
 rm -f $LOCALSTORE/*.$DATE.sql
@@ -51,20 +51,21 @@ echo "$(date +"%D %r") - Local clean up Complete!"
 # first we need to do some testing to see if the folders we need exist
 # first the year
 echo "$(date +"%D %r") - Creating Remote Backup Directory"
-ssh $REMOTEUSER@$REMOTEHOST << EOF
-if [ -d "$REMOTEDIRECTORY/$YEAR" ]; then
-mkdir $REMOTEDIRECTORY/$YEAR
+if (ssh $REMOTEUSER@$REMOTEHOST "! [ -d $REMOTEDIRECTORY/$YEAR ]"); then
+	ssh $REMOTEUSER@$REMOTEHOST "mkdir $REMOTEDIRECTORY/$YEAR"
+	echo "$(date +"%D %r") - Created $REMOTEDIRECTORY/$YEAR directory on remote host"
 fi
-if [ -d "$REMOTEDIRECTORY/$YEAR/$MONTH" ]; then
-mkdir $REMOTEDIRECTORY/$YEAR/$MONTH
+if (ssh $REMOTEUSER@$REMOTEHOST "! [ -d $REMOTEDIRECTORY/$YEAR/$MONTH ]"); then
+        ssh $REMOTEUSER@$REMOTEHOST "mkdir $REMOTEDIRECTORY/$YEAR/$MONTH"
+        echo "$(date +"%D %r") - Created $REMOTEDIRECTORY/$YEAR/$MONTH directory on remote host"
 fi
-if [ -d "$REMOTDIRECTORY/$YEAR/$MONTH/$DAY" ]; then
-mkdir $REMOTEDIRECTORY/$YEAR/$MONTH/$DAY
+if (ssh $REMOTEUSER@$REMOTEHOST "! [ -d $REMOTEDIRECTORY/$YEAR/$MONTH/$DAY ]"); then
+        ssh $REMOTEUSER@$REMOTEHOST "mkdir $REMOTEDIRECTORY/$YEAR/$MONTH/$DAY"
+        echo "$(date +"%D %r") - Created $REMOTEDIRECTORY/$YEAR/$MONT$REMOTEDIRECTORY/$YEAR/$MONTH/$DAYY directory on remote host"
 fi
-EOF
 echo "$(date +"%D %r") - Remote Backup Directory Created"
 
 echo "$(date +"%D %r") - Begin rsync of backup to remote host"
-rsync -azh $LOCALSTORE/backup_$DATE.tar.gz $REMOTEUSER@$REMOTEHOST:$REMOTEDIRECTORY/$YEAR/$MONTH/$DAY/ && echo "$(date +"%D %r") - rsync completed sucessfuly" || echo "$(date ="%D %r") - rsync failed"
+rsync -azh $LOCALSTORE/backup_$DATE.tar.gz $REMOTEUSER@$REMOTEHOST:$REMOTEDIRECTORY/$YEAR/$MONTH/$DAY/ && echo "$(date +"%D %r") - rsync completed sucessfuly" || echo "$(date +"%D %r") - rsync failed"
 
 echo "$(date +"%D %r") - Backup Complete!"
